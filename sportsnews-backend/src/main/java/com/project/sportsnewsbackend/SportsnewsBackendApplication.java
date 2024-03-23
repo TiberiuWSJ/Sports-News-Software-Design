d85561e8d281c77fc603d0efa5cacfcbd59513b5
TiberiuWSJ <96244392+TiberiuWSJ@users.noreply.github.com>
2024-03-23 14:27:24 +0200

    started spring app

---
+++ sportsnews-backend/src/main/java/com/project/sportsnewsbackend/SportsnewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.sportsnewsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsnewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsnewsBackendApplication.class, args);
+	}
+
+}
